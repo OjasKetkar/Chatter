e0360595acd285ed9fe682539aa446885527e550
Ojas <ketkarojas@gmail.com>
2024-04-30 00:02:46 +0530

    Uploadind files for ec2

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
-# from flask import Flask, render_template, request, jsonify
-# from gradio_client import Client
+from flask import Flask, render_template, request, jsonify
+from gradio_client import Client
 
-# app = Flask(__name__)
-
-# # Initialize Gradio client
-# gradio_client = Client("https://bmhchat-mentalhealth-llama-chat.hf.space/--replicas/41vla/")
-
-# # Define the main route
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# # Define the chatbot API endpoint
-# @app.route('/chatbot', methods=['POST'])
-# def chatbot():
-#     if request.method == 'POST':
-#         # Get user input from the form
-#         user_input = request.form['user_input']
-
-#         # Make a request to the chatbot API
-#         result = gradio_client.predict(user_input, api_name="/chat")
-
-#         # Return the chatbot response
-#         return jsonify({'response': result})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# from flask import Flask, render_template, request, jsonify
-# from gradio_client import Client
-
-# app = Flask(__name__)
+app = Flask(__name__)
 
 # Initialize Gradio client
-gradio_client = Client("https://phoen1x-mixtral-46-7b-chat.hf.space/--replicas/9y1c6/")
+gradio_client = Client("https://f412ebc29f3e7fd571.gradio.live/")
 
 # Store conversation history
 conversation_history = []
@@ -63,5 +33,5 @@ def chatbot():
         # Return the conversation history as JSON
         return jsonify({'conversation': conversation_history})
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '_main_':
+    app.run(debug=True)
