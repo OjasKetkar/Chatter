1bb2793c578492e813e92bf9beea436e32e19af8
Ojas <ketkarojas@gmail.com>
2024-05-22 20:21:17 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, jsonify
-from gradio_client import Client
+from openai import OpenAI
+from os import getenv
 
 app = Flask(__name__)
 
-# Initialize Gradio client
-gradio_client = Client("https://phoen1x-mixtral-46-7b-chat.hf.space/--replicas/tsgci/")
+# Initialize OpenAI client with the API key
+client = OpenAI(
+    base_url="ENTER_OPENROUTER_URL",
+    api_key="ENTER_OPENROUTER_API_KEY",
+)
 
 # Store conversation history
 conversation_history = []
@@ -15,7 +19,7 @@ def index():
     return render_template('index.html')
 
 # Define the chatbot API endpoint
-@app.route('/chatbot', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def chatbot():
     if request.method == 'POST':
         # Get user input from the form
@@ -25,13 +29,26 @@ def chatbot():
         conversation_history.append({'user': user_input})
 
         # Make a request to the chatbot API
-        result = gradio_client.predict(user_input, api_name="/chat")
+        completion = client.chat.completions.create(
+            # extra_headers={
+            #     "HTTP-Referer": "$YOUR_SITE_URL",  # Optional, for including your app on openrouter.ai rankings.
+            #     "X-Title": "$YOUR_APP_NAME",  # Optional. Shows in rankings on openrouter.ai.
+            # },
+            model="nousresearch/nous-capybara-7b:free",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                },
+            ],
+        )
+        bot_response = completion.choices[0].message.content
 
         # Update conversation history with chatbot response
-        conversation_history.append({'bot': result})
+        conversation_history.append({'bot': bot_response})
 
         # Return the conversation history as JSON
         return jsonify({'conversation': conversation_history})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0" ,debug=True)
